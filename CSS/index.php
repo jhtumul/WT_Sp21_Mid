872c86053921034aa21a3eb339b0a4b44da1dee8
jhtumul <jhtumul@gmail.com>
2021-02-27 23:01:08 +0600

    CSS basic taught in class

---
+++ CSS/index.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+        <style>
+            #p1 {color:white;
+            text-align:left;
+            background-color:#0fda86;
+            font-family:verdana;
+            }
+            #hd1,#p0,#sp1 {
+                background-color:red;
+            }
+            
+            #a1 {
+                color: mediumblue;
+                background-color: darkturquoise;
+                text-decoration: none;
+                text-align: center;
+                padding: 5px;
+                margin: 5px;
+                border: 3px solid magenta;
+                border-radius: 3px;
+            }
+            #a1:hover{
+                background-color:cornflowerblue;
+            }
+            .tomato-white{
+                color:white;
+                background-color:tomato;
+            }
+            .verdana{
+                font-family:verdana;
+            }
+        </style>
+    </head>
+    <body>
+        <h1 id="hd1">This is h1</h1>
+        <span id="sp1">This is span</span><a id="a1" href="https://www.aiub.edu" target="blank">AIUB</a>
+        <p id="p0">Roger Kemp, president of RK Media will lead our monthly webinar as he shares how to
+           effectively write for radio and television.Rural Literacy Solutions is a non-compulsory 
+           and an extra-curricula education. This Community-based Organization, is a practical 
+           response to the diagnosed high rate of illiteracy in Ghana especially the rural areas 
+           of the Northern part of the country</p>
+        <p id="p1">Roger Kemp, president of RK Media will lead our monthly webinar as he shares how to
+           effectively write for radio and television.Rural Literacy Solutions is a non-compulsory 
+           and an extra-curricula education. This Community-based Organization, is a practical 
+           response to the diagnosed high rate of illiteracy in Ghana especially the rural areas 
+           of the Northern part of the country</p>
+        <p id="p2" class="tomato-white verdana">Roger Kemp, president of RK Media will lead our monthly webinar as he shares how to
+           effectively write for radio and television.Rural Literacy Solutions is a non-compulsory 
+           and an extra-curricula education. This Community-based Organization, is a practical 
+           response to the diagnosed high rate of illiteracy in Ghana especially the rural areas 
+           of the Northern part of the country</p>
+        <p id="p3" class="tomato-white">Roger Kemp, president of RK Media will lead our monthly webinar as he shares how to
+           effectively write for radio and television.Rural Literacy Solutions is a non-compulsory 
+           and an extra-curricula education. This Community-based Organization, is a practical 
+           response to the diagnosed high rate of illiteracy in Ghana especially the rural areas 
+           of the Northern part of the country</p>
+
+    </body>
+</html>
