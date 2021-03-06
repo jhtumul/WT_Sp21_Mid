53f4300341e8899f8d14bd0d3b8b2913ed7e4e80
jhtumul <jhtumul@gmail.com>
2021-03-06 19:53:52 +0600

    demo project and lab5

---
+++ Lab5/dashboard.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../CSS/styles/project.css">
+    </head>
+    <body>
+        <div class="header"></div>
+        <div class="left-menu">
+            <ul class="plain-bullet">
+                <li><a href="">CSS HOME</a></li>
+                <li><a href="">CSS COLOR</a></li>
+                <li><a href="">CSS INDEX</a></li>
+            </ul>
+        </div>
+    </body>
+    <div class="footer"></div>
+</html>
+
+        
