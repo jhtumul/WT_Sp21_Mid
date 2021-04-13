a960f26f26e3fd17d25ccb44ed2b6752fe64dd61
jhtumul <jhtumul@gmail.com>
2021-04-13 12:28:02 +0600

    updating lastsaved project

--- lastSavedProject/3[current]/loggedInButton.php
+++
@@ -1,5 +0,0 @@
-<head>
-<link rel="stylesheet" href="styles/basicLayout.css">
-</head>
-<li><a href="myAccount.php" class="right">My Account</a></li>
-<li><a href="logout.php" class="right">Logout</a></li>
